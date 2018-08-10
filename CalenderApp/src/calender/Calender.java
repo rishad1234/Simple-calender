898b8ac320a636549f52bd397e70bb3461c88ef3
Rishadul Islam <rishadulislamkhan@gmail.com>
2018-08-10 19:01:02 +0600

    icon added

--- CalenderApp/src/calender/Calender.java
+++ CalenderApp/src/calender/Calender.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -28,6 +29,7 @@ public class Calender extends Application {
         
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.getIcons().add(new Image("/images/icon.png"));
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
     }
